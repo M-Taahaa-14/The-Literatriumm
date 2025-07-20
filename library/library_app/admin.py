6ccb33f1c3ab0126c4d63efab13bb9d382d9ca03
M-Taahaa-14 <mr.taaha16@gmail.com>
2025-07-20 00:24:17 -0700

    Clean repository

--- library/library_app/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Book, BookCategory, BorrowRecord, UserProfile
-
-admin.site.register(Book)
-admin.site.register(BookCategory)
-admin.site.register(BorrowRecord)
-admin.site.register(UserProfile)
-
